aef4abef075a156c8019b7a62dc498e982c1d388
losemilos <los.emilos.email@gmail.com>
2016-06-13 03:19:32 +0200

    Test

---
+++ src/it/blachnio/Main.java
@@ -0,0 +1,10 @@
+package it.blachnio;
+
+/**
+ * Created by user on 6/13/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
